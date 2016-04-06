034972ab67ec6140575870bb59c915f7ba9dfa85
sashastip <saschastipanenko@gmail.com>
2016-04-06 14:28:26 +0300

    added api routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   root to: 'welcome#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :books
+    end
+  end
 end
